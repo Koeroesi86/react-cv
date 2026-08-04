e312fdb17c437bf75a4dc77fb3d75815e085134e
Krisztian Korosi <koeroesi86@gmail.com>
2026-08-04 10:49:54 +0200

    lint fix

--- src/theme/utils/createFileName.ts
+++ src/theme/utils/createFileName.ts
@@ -1,8 +1,6 @@
-const createFileName = (name: string): string => {
-  return name
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[ ]/, ".");
-};
+const createFileName = (name: string): string => name
+  .normalize('NFD')
+  .replace(/[\u0300-\u036f]/g, "")
+  .replace(/[ ]/, ".");
 
 export default createFileName;
